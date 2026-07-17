95dce5acc5b50248c7a449249856636c1b6e53d5
Suelane Garcia Fontes <suelanegarcia@gmail.com>
2026-07-17 17:39:16 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Cole aqui os dados públicos do seu projeto Supabase.
 // Supabase > Project Settings > API
 window.RISKLAB_CONFIG = {
-  supabaseUrl: "https://kewmaegxvrzkbylsjfif.supabase.co",
+  supabaseUrl: "https://kewmaegxvrzkbylsjfjf.supabase.co/",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtld21hZWd4dnJ6a2J5bHNqZmpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyNDUyMTgsImV4cCI6MjA5OTgyMTIxOH0.bjdpoT7xIJ06K3j1E2VRS_6_8rqIkNTUTz5BXVaMuqU"
 };
